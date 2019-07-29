0e01f47b7b28c3baa415609ea0db914994468bbc
OshriHalimi <41571133+OshriHalimi@users.noreply.github.com>
2019-07-29 12:30:39 +0300

    Add files via upload

--- Learning Correspondence of Synthetic Shapes/plot_training_error.m
+++ Learning Correspondence of Synthetic Shapes/plot_training_error.m
@@ -3,8 +3,11 @@ load('.\Results\train_faust_synthetic\training_error.mat')
 title('Unsupervised Training Process')
 yyaxis left
 semilogy(error_vec_unsupervised);
+ylabel('Unsupervised Loss')
 
 yyaxis right
 semilogy(error_vec_supervised);
+ylabel('Supervised Loss')
 
 xlabel('Number of mini-batches')
+legend({'Unsupervised','Supervised'})
